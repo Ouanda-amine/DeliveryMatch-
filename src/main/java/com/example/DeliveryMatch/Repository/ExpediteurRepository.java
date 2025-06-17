9257c6c9fca3c0cfdc0bbb7f4f58075c6eb95449
amiine <amineouanda@gmail.com>
2025-06-17 18:03:57 +0100

    partie expediteur sans controlleur

---
+++ src/main/java/com/example/DeliveryMatch/Repository/ExpediteurRepository.java
@@ -0,0 +1,7 @@
+package com.example.DeliveryMatch.Repository;
+
+import com.example.DeliveryMatch.Model.Expediteur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpediteurRepository extends JpaRepository<Expediteur , Long> {
+}
